f6c694f104a44bcf7055c1ab851bc6f5d0c7fed2
MyungJiwoo <1206jiwoo@gmail.com>
2022-03-24 12:21:23 +0900

    0324 - q2 보충

--- 08/js/q2.js
+++ 08/js/q2.js
@@ -1,7 +1,18 @@
 var checking = document.querySelectorAll(".check");
+var onoff = [];
+for (var i = 0; i < checking.length; i++) {
+    onoff[i] = true;
+}
 
 for (var i = 0; i < checking.length; i++) {
     checking[i].addEventListener("click", function () {
-        this.parentNode.style.color = "#ccc";
+        if (onoff[i]) {
+            this.parentNode.style.color = "#ccc";
+            onoff[i] = false;
+        }
+        else {
+            this.parentNode.style.color = "";
+            onoff[i] = true;
+        }
     });
 }
